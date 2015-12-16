0ecfaa9b35c33bc96d82a95d1cd9caa7ea8c8ad6
connor-occhialini <connor.occhialini@uconn.edu>
2015-12-15 19:21:43 -0500

    third commit

--- pi-poly-u.c
+++ pi-poly-u.c
@@ -16,14 +16,18 @@ int main()
 
     i = 0;
     n = 4;
+
+    //New Inital condition (inverse of original
     s = sqrt(2);
     p = n/s;
     printf("%3d   %10ld   %.14f    % .14f    % .14f\n", i, n, s, p, M_PI-p);
     
     for(i = 1; i < N; i++) 
     {
+	//New recursion relation bewteen 1/sin(a/2) and 1/sin(a)
         s = sqrt(2*s*s + 2*sqrt(s*s*s*s - s*s));
         n *= 2;
+	//New definition for the Pi approximation
         p = n/s;
         printf("%3d   %10ld   %.14f    % .14f    % .14f\n", i, n, s, p, M_PI-p);
     }
